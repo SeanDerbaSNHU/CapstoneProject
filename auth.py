9bad3f96072782911ff6874c1bdf809ea32d0e0f
seand <sean.derba@snhu.edu>
2022-10-11 16:02:22 -0400

    vjkbaw

--- auth.py
+++ auth.py
@@ -1,12 +1,14 @@
+# This file is used for setting up web pages, all addresses should be put here
 from flask import Flask, render_template, Blueprint, redirect, url_for, request
+from app import db
 
 auth = Blueprint('auth', __name__)
 
 
 # use decorators to link the function to a url
 @auth.route('/')
-def home():
-    return "Hello, World!"  # return a string
+def index():
+    return render_template('index.html')  # return a string
 
 @auth.route('/welcome')
 def welcome():
@@ -22,3 +24,15 @@ def login():
         else:
             return redirect('/welcome')
     return render_template('login.html', error=error)
+
+@auth.route('/register')
+def register():
+    return render_template('register.html')
+
+@auth.route('/logout')
+def logout():
+    return 'Logout'
+
+@auth.route('/profile')
+def profile():
+    return render_template('profile.html')
