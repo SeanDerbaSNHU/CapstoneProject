9c35f25077f922e174995516ecf38ceec8b99a7f
Nicholas Farone <65373120+cardboard-c@users.noreply.github.com>
2022-10-12 00:11:42 -0400

    Functional Display in HTLM from Back End

--- Function.py
+++ Function.py
@@ -1,8 +1,9 @@
 import statsapi
 import logging
 def getplayer(name, yearlyOrCareer, pos):
-    stat = statsapi.player_stats(next(x['id'] for x in statsapi.get('sports_players', {'season': 2022, 'gameType': 'W'})['people'] if x['fullName'] == name), pos, yearlyOrCareer)
-    stat = stat.split()[7:];
+    #stat = statsapi.player_stats(next(x['id'] for x in statsapi.get('sports_players', {'season': 2022, 'gameType': 'W'})['people'] if x['fullName'] == name), pos, yearlyOrCareer)
+    stat = statsapi.player_stat_data(next(x['id'] for x in statsapi.get('sports_players', {'season': 2022, 'gameType': 'W'})['people'] if x['fullName'] == name), pos, yearlyOrCareer, sportId=1)
+    #stat = stat.split()[7:];
     return stat;
 
 def getSepcificStat(stats, statName):
@@ -15,3 +16,33 @@ def getSepcificStat(stats, statName):
     print(stats_dict.keys())
     final = statName + " :" + stats_dict[statName + ':']
     return final
+def getSepcificStatNum(stats, statName):
+    index = 0
+    stats_dict = {}
+
+    while index < len(stats):
+        stats_dict[stats[index]] = stats[index + 1]
+        index += 2
+    print(stats_dict.keys())
+    final = stats_dict[statName + ':']
+    return final
+
+def compare(name1, name2, statName):
+    p1 = getplayer(name1,"career", "hitting")
+    p2 = getplayer(name2,"career", "hitting")
+    p1_stat = getSepcificStatNum(p1,statName)
+    p2_stat = getSepcificStatNum(p2,statName)
+    differnce = abs(int(p1_stat) - int(p2_stat))
+    if(p1_stat > p2_stat):
+        print(name1 + " has " + str(differnce) + " more " + statName +" than " + name2)
+    elif(p1_stat < p2_stat):
+        print(name2 + " has " + str(differnce) + " more " + statName + " than " + name1)
+
+def compareToAverage(name1):
+    p1 = getplayer(name1, "career", "hitting")
+    p1_stat = float(getSepcificStatNum(p1, "avg"))
+    differnce = abs(p1_stat - .243)
+    if (p1_stat > .243):
+        print(name1 + " has " + str(differnce) + " more than average")
+    elif (p1_stat < .243):
+        print(name1 + " has " + str(differnce) + " less than average")
